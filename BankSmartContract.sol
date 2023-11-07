bcdf0b9c4b3178218509c0f05285c56c4096dc71
Rohit Bhandare <92716110+RohitBhandare@users.noreply.github.com>
2023-11-07 20:54:23 +0530

    Update BankSmartContract.sol

--- BankSmartContract.sol
+++ BankSmartContract.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Bank {
+    int private balance; 
+
+    constructor() {
+        balance = 0;  
+    }
+
+    function getBalance() public view returns (int) {
+        return balance;
+    }
+
+    function deposit(int amount) public {
+        require(amount > 0, "Amount must be greater than 0");
+        balance += amount;
+    }
+
+    function withdraw(int amount) public {
+        require(amount > 0, "Amount must be greater than 0");
+        require(amount <= balance, "Insufficient balance");
+        balance -= amount;
+    }
+}
 
